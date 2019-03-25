c00726745e398c10735b5ee544f0a314b4f6c138
277825228 <277825228@qq.com>
2019-03-25 15:33:33 +0800

    commit project

---
+++ flow-rest/script/data-clear.sql
@@ -0,0 +1,26 @@
+delete from act_ru_variable;
+delete from act_re_model;
+delete from act_ge_bytearray;
+delete from act_hi_actinst;
+delete from act_hi_identitylink;
+delete from act_hi_procinst;
+delete from act_hi_taskinst;
+delete from act_hi_varinst;
+delete from act_procdef_info;
+delete from act_re_deployment;
+delete from act_ru_identitylink;
+delete from act_ru_task;
+delete from act_ru_variable;
+delete from act_ru_execution;
+delete from act_re_procdef;
+delete from order_inst;
+delete from act_hi_comment;
+delete from act_de_model;
+delete from act_id_membership;
+delete from act_id_group;
+delete from act_id_user;
+
+
+
+
+
