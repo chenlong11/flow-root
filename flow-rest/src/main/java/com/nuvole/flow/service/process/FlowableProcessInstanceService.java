d782f38ff85e14f34dc2c53957c320393e8b501d
277825228 <277825228@qq.com>
2019-03-26 17:16:00 +0800

    commit project

--- flow-rest/src/main/java/com/nuvole/flow/service/process/FlowableProcessInstanceService.java
+++ flow-rest/src/main/java/com/nuvole/flow/service/process/FlowableProcessInstanceService.java
@@ -8,7 +8,6 @@ import org.flowable.engine.RuntimeService;
 import org.flowable.engine.history.HistoricProcessInstance;
 import org.flowable.engine.impl.persistence.entity.ProcessDefinitionEntity;
 import org.flowable.form.api.FormInfo;
-import org.flowable.form.api.FormRepositoryService;
 import org.flowable.form.api.FormService;
 import org.flowable.idm.api.IdmIdentityService;
 import org.flowable.idm.api.User;
@@ -33,9 +32,6 @@ public class FlowableProcessInstanceService {
 
     @Autowired
     protected FormService formService;
-
-    @Autowired
-    protected FormRepositoryService formRepositoryService;
 
     @Autowired
     protected ContentService contentService;
